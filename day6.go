904369d0cbb10163cc7510b6201bbd37a38f3376
love <kshitizluv123@gmail.com>
2025-11-26 12:13:52 +0545

    recursion

--- day6.go
+++ day6.go
@@ -4,6 +4,13 @@ package main
 
 import "fmt"
 
+func fact(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * fact(n-1)
+}
+
 func main() {
 	fmt.Println("recursion")
 
@@ -19,5 +26,24 @@ func main() {
 
 	fmt.Println("value of ptr ", ptr)  // only have address , have same address as number i.e.: ptr, number == same address
 	fmt.Println("value of ptr ", *ptr) // actually points to address value
-	print(&number)                     // memory address
+	print(&number, "\n")               // memory address
+
+	// ********
+	fmt.Println("****************************************")
+
+	data := fact(6)
+	fmt.Println("factorial of 6: ", data)
+
+	var fib func(n int) int
+
+	fib = func(n int) int {
+		if n < 2 {
+			return n
+		}
+
+		return fib(n-1) + fib(n-2)
+	}
+
+	fmt.Print(fib(6))
+
 }
